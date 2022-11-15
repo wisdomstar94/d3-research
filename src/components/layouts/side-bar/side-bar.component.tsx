0971f76db641df427a9e10bc2e8f4e46f7e17600
wisdomstar94 <wisdomstar94@gmail.com>
2022-11-15 09:31:50 +0900

    d3-156-geoConicEquidistantRaw 페이지 작성.

--- src/components/layouts/side-bar/side-bar.component.tsx
+++ src/components/layouts/side-bar/side-bar.component.tsx
@@ -167,6 +167,7 @@ const SideBar = (props: ISideBar.Props) => {
     { menuName: 'd3-153-geoConicEqualArea', menuLink: '/d3-example/d3-153-geoConicEqualArea' },
     { menuName: 'd3-154-geoConicEqualAreaRaw', menuLink: '/d3-example/d3-154-geoConicEqualAreaRaw' },
     { menuName: 'd3-155-geoConicEquidistant', menuLink: '/d3-example/d3-155-geoConicEquidistant' },
+    { menuName: 'd3-156-geoConicEquidistantRaw', menuLink: '/d3-example/d3-156-geoConicEquidistantRaw' },
   ]);
 
   useEffect(() => {
