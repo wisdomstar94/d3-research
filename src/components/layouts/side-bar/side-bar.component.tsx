b3480ffacec3bbd63acad4100317d1828cbd7158
wisdomstar94 <wisdomstar94@gmail.com>
2022-11-16 14:29:38 +0900

    d3-175-geoOrthographicRaw 페이지 작성.

--- src/components/layouts/side-bar/side-bar.component.tsx
+++ src/components/layouts/side-bar/side-bar.component.tsx
@@ -186,6 +186,7 @@ const SideBar = (props: ISideBar.Props) => {
     { menuName: 'd3-172-geoNaturalEarth1', menuLink: '/d3-example/d3-172-geoNaturalEarth1' },
     { menuName: 'd3-173-geoNaturalEarth1Raw', menuLink: '/d3-example/d3-173-geoNaturalEarth1Raw' },
     { menuName: 'd3-174-geoOrthographic', menuLink: '/d3-example/d3-174-geoOrthographic' },
+    { menuName: 'd3-175-geoOrthographicRaw', menuLink: '/d3-example/d3-175-geoOrthographicRaw' },
     
   ]);
 
