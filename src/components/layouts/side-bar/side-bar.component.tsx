efb063eb24e5f46df3679b3750e6a48a2d40b940
wisdomstar94 <wisdomstar94@gmail.com>
2022-11-16 16:22:12 +0900

    d3-186-gray 페이지 작성.

--- src/components/layouts/side-bar/side-bar.component.tsx
+++ src/components/layouts/side-bar/side-bar.component.tsx
@@ -197,6 +197,7 @@ const SideBar = (props: ISideBar.Props) => {
     { menuName: 'd3-183-geoTransform', menuLink: '/d3-example/d3-183-geoTransform' },
     { menuName: 'd3-184-geoTransverseMercator', menuLink: '/d3-example/d3-184-geoTransverseMercator' },
     { menuName: 'd3-185-geoTransverseMercatorRaw', menuLink: '/d3-example/d3-185-geoTransverseMercatorRaw' },
+    { menuName: 'd3-186-gray', menuLink: '/d3-example/d3-186-gray' },
     
   ]);
 
