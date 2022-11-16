e992948000051947b440b39c8d3743a004c823f4
wisdomstar94 <wisdomstar94@gmail.com>
2022-11-16 13:09:41 +0900

    d3-167-geoIdentity 페이지 작성.

---
+++ src/pages/d3-example/d3-167-geoIdentity/index.tsx
@@ -0,0 +1,78 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import { useEffect, useRef } from "react";
+import CommonLayout from "../../../components/layouts/common-layout/common-layout.component";
+import { select, json, geoPath, geoAzimuthalEqualArea, geoIdentity } from "d3";
+
+// https://stackoverflow.com/questions/42430361/scaling-d3-v4-map-to-fit-svg-or-at-all
+
+const Index: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>d3-167-geoIdentity</title>
+        <meta name="description" content="d3-167-geoIdentity 예시 코드 페이지입니다." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <CommonLayout>
+        <PageContents />
+      </CommonLayout>
+    </>
+  );
+};
+
+const PageContents = () => {
+  const boxElementRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    while (boxElementRef.current?.hasChildNodes() === true) {
+      if (boxElementRef.current?.firstChild !== null) {
+        boxElementRef.current?.removeChild(boxElementRef.current?.firstChild);
+      }
+    }
+
+    /*
+      
+    */
+
+    const width = 800;
+    const height = 800;
+
+    const svg = select(boxElementRef.current)
+      .append('svg')
+      .attr("width", width)
+      .attr("height", height);
+
+    const targetUrl = [
+      '/geo/skorea-municipalities-2018-geo.json',
+      '/geo/gz_2010_us_050_00_5m.json',
+      '/geo/countries.geojson.json', // https://github.com/datasets/geo-countries/blob/master/data/countries.geojson
+    ];
+
+    json(targetUrl[2]).then((data: any) => {
+      const projection = geoIdentity()
+        .fitExtent([[0, 0], [width, height]], data);
+        // .scale(width / 1.5 / Math.PI)
+        // .translate([width / 2, height / 2]);
+
+      svg.append("g")
+        .selectAll("path")
+        .data(data.features)
+        .enter().append("path")
+        .attr("fill", "green")
+        .attr("d", geoPath().projection(projection) as any)
+        .style("stroke", "#ffff");
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="box" ref={boxElementRef}>
+      
+      </div>
+    </>
+  );
+};
+
+export default Index;
