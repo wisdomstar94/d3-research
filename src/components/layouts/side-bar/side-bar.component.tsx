05e623b33b9e7439153ee63444169a5c1bf1d217
wisdomstar94 <wisdomstar94@gmail.com>
2023-01-15 00:12:16 +0900

    d3-357-scalePow 페이지 작성 완료.

--- src/components/layouts/side-bar/side-bar.component.tsx
+++ src/components/layouts/side-bar/side-bar.component.tsx
@@ -371,6 +371,7 @@ const SideBar = (props: ISideBar.Props) => {
     { menuName: 'd3-354-scaleLog', menuLink: '/d3-example/d3-354-scaleLog' },
     { menuName: 'd3-355-scaleOrdinal', menuLink: '/d3-example/d3-355-scaleOrdinal' },
     { menuName: 'd3-356-scalePoint', menuLink: '/d3-example/d3-356-scalePoint' },
+    { menuName: 'd3-357-scalePow', menuLink: '/d3-example/d3-357-scalePow' },
     
   ]);
 
