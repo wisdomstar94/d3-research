d50db0dba7d766ca0b380eade68c32e931c98456
wisdomstar94 <wisdomstar94@gmail.com>
2023-01-16 10:52:50 +0900

    d3-458-timeYear 페이지 작성 완료.

--- src/components/layouts/side-bar/side-bar.component.tsx
+++ src/components/layouts/side-bar/side-bar.component.tsx
@@ -472,6 +472,7 @@ const SideBar = (props: ISideBar.Props) => {
     { menuName: 'd3-455-timeWednesdays', menuLink: '/d3-example/d3-455-timeWednesdays' },
     { menuName: 'd3-456-timeWeek', menuLink: '/d3-example/d3-456-timeWeek' },
     { menuName: 'd3-457-timeWeeks', menuLink: '/d3-example/d3-457-timeWeeks' },
+    { menuName: 'd3-458-timeYear', menuLink: '/d3-example/d3-458-timeYear' },
   ]);
   
 
