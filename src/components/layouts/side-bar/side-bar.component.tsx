1b237c34de44a55bf04f36fbf3cc42bcfb266781
wisdomstar94 <wisdomstar94@gmail.com>
2023-01-19 13:37:12 +0900

    d3-501-utcSeconds 페이지 작성 완료.

--- src/components/layouts/side-bar/side-bar.component.tsx
+++ src/components/layouts/side-bar/side-bar.component.tsx
@@ -515,6 +515,7 @@ const SideBar = (props: ISideBar.Props) => {
     { menuName: 'd3-498-utcSaturday', menuLink: '/d3-example/d3-498-utcSaturday' },
     { menuName: 'd3-499-utcSaturdays', menuLink: '/d3-example/d3-499-utcSaturdays' },
     { menuName: 'd3-500-utcSecond', menuLink: '/d3-example/d3-500-utcSecond' },
+    { menuName: 'd3-501-utcSeconds', menuLink: '/d3-example/d3-501-utcSeconds' },
     
   ]);
   
