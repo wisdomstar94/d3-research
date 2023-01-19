7611daf3bf0c5a48507e321ee477cf0479497123
wisdomstar94 <wisdomstar94@gmail.com>
2023-01-19 13:24:47 +0900

    d3-495-utcMonth 페이지 작성 완료.

---
+++ src/app/d3-example/d3-495-utcMonth/page.tsx
@@ -0,0 +1,50 @@
+"use client"
+import { utcMonth } from "d3";
+import { NextPage } from "next";
+import Head from "next/head";
+import { useEffect, useRef } from "react";
+import CommonLayout from "../../../components/layouts/common-layout/common-layout.component";
+
+const Index: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>d3-495-utcMonth</title>
+        <meta name="description" content="d3-495-utcMonth 예시 코드 페이지입니다." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <CommonLayout>
+        <PageContents />
+      </CommonLayout>
+    </>
+  );
+};
+
+const PageContents = () => {
+  const boxElementRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    while (boxElementRef.current?.hasChildNodes() === true) {
+      if (boxElementRef.current?.firstChild !== null) {
+        boxElementRef.current?.removeChild(boxElementRef.current?.firstChild);
+      }
+    }
+
+    /*
+      d3.utcMonth 함수는 월(예: 2012년 2월 1일 오전 12시). 범위는 28일에서 31일입니다.
+    */
+    const result = utcMonth(new Date);
+    console.log(`result`, result);
+  }, []);
+
+  return (
+    <>
+      <div className="box" ref={boxElementRef}>
+      
+      </div>
+    </>
+  );
+};
+
+export default Index;
